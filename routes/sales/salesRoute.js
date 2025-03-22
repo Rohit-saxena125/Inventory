13099aae00f118725f2ee0a4d2eebc7e10020bd2
Rohit-saxena125 <rohitsaxena125@gmail.com>
2025-03-22 14:05:57 +0530

    fix

--- routes/sales/salesRoute.js
+++ routes/sales/salesRoute.js
@@ -4,5 +4,8 @@ const { tokenVerify,checkRole } = require('../../middlewares/authMiddleware');
 
 router.get('/fetchSales', tokenVerify, checkRole(['Admin']), salesController.fetchSales);
 router.get('/fetchSales/:id', tokenVerify, checkRole(['Admin']), salesController.fetchSalesById);
+router.post('/createSales', tokenVerify, checkRole(['Admin','Cashier']), salesController.createSales);
+router.patch('/updateSales/:id', tokenVerify, checkRole(['Admin']), salesController.updateSales);
+router.patch('/deleteSales/:id', tokenVerify, checkRole(['Admin']), salesController.deleteSales);
 
 module.exports = router;
