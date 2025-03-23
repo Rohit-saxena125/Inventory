460ccfba5fd5bad522e83431262b1ff79fc06e1e
Rohit-saxena125 <rohitsaxena125@gmail.com>
2025-03-23 11:28:47 +0530

    Fix

--- middlewares/upload/upload.js
+++ middlewares/upload/upload.js
@@ -1,6 +1,8 @@
 const multer = require("multer");
 const multerS3 = require("multer-s3");
-const { S3Client } = require("@aws-sdk/client-s3");
+const { S3Client,PutObjectCommand} = require("@aws-sdk/client-s3");
+const fs = require("fs");
+const path = require("path");
 
 const s3Client = new S3Client({
   region: process.env.Region,
