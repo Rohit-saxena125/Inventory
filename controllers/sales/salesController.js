a402e46fbd11f91b0a2860654dd8e8ca6a971019
Rohit-saxena125 <rohitsaxena125@gmail.com>
2025-04-09 00:31:39 +0530

    Fix

--- controllers/sales/salesController.js
+++ controllers/sales/salesController.js
@@ -303,7 +303,7 @@ exports.deleteSalesAll = async (req, res) => {
     if (userId) {
       query.createdBy = userId;
     }
-    await Sale.updateManyMany(query,{ $set: { isDeleted: true } }, { new: true, runValidators: true });
+    await Sale.updateMany(query,{ $set: { isDeleted: true } }, { new: true, runValidators: true });
     return successResponse(res, 'All Sales deleted successfully ');
   } catch (error) {
     return internalServerErrorResponse(res, error);
