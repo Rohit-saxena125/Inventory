a2f7c841b449dc506f5e13abfb21da8c14ef0047
Rohit-saxena125 <rohitsaxena125@gmail.com>
2025-04-20 11:13:42 +0530

    Fix

--- controllers/sales/salesController.js
+++ controllers/sales/salesController.js
@@ -551,7 +551,7 @@ async function createInvoicePDF(invoiceDataArray, outputPath) {
     // Table Heading
     doc
       .font('Courier-Bold')
-      .text('Item             Qty             Rate             Amt')
+      .text('Item        Qty       Rate       Amt')
       .font('Courier');
 
     // Items Loop
