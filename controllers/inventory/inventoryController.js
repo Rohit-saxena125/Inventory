07254730bc7f4f901901acd3e6a935b119b7a374
Rohit-saxena125 <rohitsaxena125@gmail.com>
2025-06-18 22:00:52 +0530

    fix

--- controllers/inventory/inventoryController.js
+++ controllers/inventory/inventoryController.js
@@ -69,10 +69,9 @@ exports.getAllInventory = async (req, res, next) => {
         $lte: moment.tz(endDate,'DD-MM-YYYY', 'Asia/Kolkata').endOf('day').utc().toDate(),
       };
     }
-    const flexibleSearch = search.replace(/[^a-zA-Z0-9]/g, '').split('').join('[\\s\\-]*');
     if (search) {
       query.itemName = {
-        $regex: flexibleSearch,
+        $regex: search,
         $options: 'i',
       };
     }
