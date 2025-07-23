13051e8ec087c893b86155e0587410001fda1e8f
Rohit-saxena125 <rohitsaxena125@gmail.com>
2025-07-23 23:49:00 +0530

    fix

--- controllers/sales/salesController.js
+++ controllers/sales/salesController.js
@@ -816,7 +816,7 @@ async function createInvoicePDF(invoiceDataArray, outputPath) {
     // Table columns config
     const columns = [
       { name: 'SN', width: 3 },
-      { name: 'Item Name', width: 15 },
+      { name: 'Item Name', width: 14 },
       { name: 'Qty', width: 6 },
       { name: 'Unit', width: 6 },
       { name: 'Rate', width: 10 },
